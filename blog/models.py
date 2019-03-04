ad1ff858da227dfc42d41d45c7882d9cd3553033
刘松 <ls@btctrade.com>
2019-03-04 15:35:18 +0800

    add templates

--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -17,4 +21,6 @@ class Article(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag,blank=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html', context={
+        'title': '我的首页',
+        'welcome': '呵呵欢迎'
+    })
