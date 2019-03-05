fa4c2fac1e4472b75a19f965a73d0700aaada904
刘松 <ls@btctrade.com>
2019-03-05 16:10:03 +0800

    fix model and tpl

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Site(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
