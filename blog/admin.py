49aec658d214fd7556f9623d76a2ff1ad3fc0c20
刘松 <ls@btctrade.com>
2019-03-05 18:46:03 +0800

    重构models

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Category,Article,ArticleType,Tag
-
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Article)
-admin.site.register(ArticleType)
-
--- blog/models.py
+++ blog/models.py
@@ -1,57 +1,26 @@
+import logging
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils.timezone import now
+from django.core.exceptions import ValidationError
+class BaseModel(models.Model):
+    '''基础模型'''
+    id = models.AutoField(primary_key=True)
+    create_time = models.DateTimeField('创建时间',default=now)
+    last_mod_time = models.DateTimeField('修改时间',default=now)
 
-class Site(models.Model):
-    title = models.CharField(max_length=50)
-    subtitle = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+class Setting(BaseModel):
+    sitename = models.CharField("网站名称",max_length=100,default='')
+    site_description = models.CharField("网站描述",max_length=200,default='')
+    site_keywords = models.TextField("网站关键字",max_length=1000,default='')
+    article_sub_length = models.IntegerField("文章摘要长度",default=300)
+    analyticscode = models.TextField("网站统计代码",max_length=1000,default='')
 
-    def __str__(self):
-        return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = '网站配置'
 
     def __str__(self):
-        return self.name
+        return self.sitename
 
-
-class ArticleType(models.Model):
-    ARTICLE_TYPE = 'ARTICLE'
-    AUDIO_TYPE = 'AUDIO'
-    CODE_TYPE = 'CODE'
-    GALLERY_TYPE = 'GALLERY'
-    VIDEO_TYPE = 'VIDEO'
-    ARTICLE_TYPE_CHOICES = (
-        (ARTICLE_TYPE, 'article'),
-        (AUDIO_TYPE, 'audio'),
-        (CODE_TYPE, 'code'),
-        (GALLERY_TYPE, 'gallery'),
-        (VIDEO_TYPE, 'video')
-    )
-    name = models.CharField(max_length=10, choices=ARTICLE_TYPE_CHOICES, default=ARTICLE_TYPE)
-
-    def __str__(self):
-        return self.name
-
-
-class Article(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    create_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-    excerpt = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    type = models.OneToOneField(ArticleType,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+    def clean(self):
+        if Setting.objects.exclude(id=self.id).count():
+            raise ValidationError(_('只能有一个配置'))
